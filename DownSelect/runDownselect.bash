de1c7722d10492a93a672e5d156838245e1b054d
Huihang Qiu <33415378+Teyunman@users.noreply.github.com>
2025-11-12 10:27:38 -0600

    Add license comments to runDownselect.bash
    Add licensing information to the runDownselect.bash script.

--- DownSelect/runDownselect.bash
+++ DownSelect/runDownselect.bash
@@ -1,3 +1,13 @@
+#
+# Copyright <year> The Board of Trustees of the University of Illinois. All Rights Reserved.
+# Licensed under the terms of the MIT license (the "License")
+# The License is included in the distribution as License.txt file.
+# You may not use this file except in compliance with the License. 
+# Software distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and limitations under the License.
+#
+
 #!/bin/bash
 
 # Define an array of directory names
